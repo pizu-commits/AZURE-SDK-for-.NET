cf2d3702f9cdd4df45e596570b9ab89ca1f11212
Mike Pateras <mipatera@microsoft.com>
2022-06-14 14:20:03 -0400

    Taskmanager working prototype

--- sdk/batch/Azure.Data.Batch/src/TaskManager/TaskManager.cs
+++ sdk/batch/Azure.Data.Batch/src/TaskManager/TaskManager.cs
@@ -3,26 +3,28 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Azure.Data.Batch.Models;
 
-namespace Azure.Data.Batch.TaskManager
+namespace Azure.Data.Batch
 {
-    internal class TaskManager
+    public class TaskManager
     {
         private JobClient jobClient;
         private TaskClient taskClient;
         private string jobId;
         private Queue<Task> pendingTasks = new Queue<Task>();
-        private Queue<Task> activeTasks = new Queue<Task>();
-        private Queue<Task> completedTasks = new Queue<Task>();
+        private List<Task> activeTasks = new List<Task>();
+        private List<Task> completedTasks = new List<Task>();
+        private int maxParrelelTasks;
 
-        public TaskManager(JobClient jobClient, TaskClient taskClient, string jobId, IEnumerable<Task> tasks)
+        public TaskManager(JobClient jobClient, TaskClient taskClient, string jobId, IEnumerable<Task> tasks, int maxParrelelTasks = 0)
         {
             this.jobClient = jobClient;
             this.taskClient = taskClient;
-
             this.jobId = jobId;
+            this.maxParrelelTasks = maxParrelelTasks;
 
             foreach (var task in tasks)
             {
@@ -30,14 +32,55 @@ namespace Azure.Data.Batch.TaskManager
             }
         }
 
-        private void CheckStatus()
+        public async System.Threading.Tasks.Task Start()
+        {
+            StartTasks();
+
+            while (IsFinished() == false)
+            {
+                await System.Threading.Tasks.Task.Delay(5000).ConfigureAwait(false);
+
+                UpdateTasks();
+                StartTasks();
+            }
+
+            return;
+        }
+
+        private void UpdateTasks()
         {
             var tasks = taskClient.ListTasks(jobId);
+
+            foreach (Task task in tasks)
+            {
+                Task activeTask = activeTasks.FirstOrDefault(t => t.Id == task.Id);
+
+                if (activeTask != null && task.State == TaskState.Completed)
+                {
+                    CompleteTask(activeTask);
+                }
+            }
         }
 
         private void StartTasks()
         {
+            while (pendingTasks.Count > 0 && (maxParrelelTasks == 0 || activeTasks.Count < maxParrelelTasks))
+            {
+                Task task = pendingTasks.Dequeue();
+                StartTask(task);
+            }
+        }
 
+        private void StartTask(Task task)
+        {
+            activeTasks.Add(task);
+            taskClient.AddTask(jobId, task);
+        }
+
+        private void CompleteTask(Task task)
+        {
+            completedTasks.Add(task);
+            activeTasks.Remove(task);
         }
 
         private bool IsFinished()
