3696ae4bcf8ac26b1697ef8ea3537eb7f4820006
juntuchen <juntuchen@microsoft.com>
2022-09-08 20:44:19 -0400

    rename

---
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/RecognizeOptions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Communication.CallAutomation
+{
+    /// <summary>
+    /// Optional elements for Recognize.
+    /// </summary>
+    public class RecognizeOptions
+    {
+        /// <summary> Initializes a new instance of RecognizeOptions. </summary>
+        /// <param name="targetParticipant"> Target participant of DTFM tone recognition. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="targetParticipant"/> is null. </exception>
+        public RecognizeOptions(CommunicationIdentifier targetParticipant)
+        {
+            if (targetParticipant == null)
+            {
+                throw new ArgumentNullException(nameof(targetParticipant));
+            }
+
+            TargetParticipant = targetParticipant;
+        }
+
+        /// <summary> Determines if we interrupt the prompt and start recognizing. </summary>
+        public bool? InterruptPrompt { get; set; }
+        /// <summary> Time to wait for first input after prompt (if any). </summary>
+        public int? InitialSilenceTimeoutInSeconds { get; set; }
+        /// <summary> Target participant of DTFM tone recognition. </summary>
+        public CommunicationIdentifier TargetParticipant { get; }
+        /// <summary> Defines configurations for DTMF. </summary>
+        public DtmfOptions DtmfOptions { get; set; }
+    }
+}
