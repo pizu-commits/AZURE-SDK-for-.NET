c5848e4697c2ba19a8f2416700955178838baab4
sima-zhu <sizhu@microsoft.com>
2022-09-12 14:53:29 -0700

    Split the packages on code generation.

--- eng/scripts/CodeChecks.ps1
+++ eng/scripts/CodeChecks.ps1
@@ -6,6 +6,9 @@ param (
     [string] $ServiceDirectory,
 
     [Parameter()]
+    [string] $ServiceToTest,
+
+    [Parameter()]
     [string] $ProjectDirectory,
 
     [Parameter()]
@@ -63,7 +66,7 @@ try {
         }
 
         Write-Host "`nChecking that solutions are up to date"
-        Join-Path "$PSScriptRoot/../../sdk" $ServiceDirectory  `
+        Join-Path "$PSScriptRoot/../../sdk" $ServiceToTest  `
             | Resolve-Path `
             | % { Get-ChildItem $_ -Filter "Azure.*.sln" -Recurse } `
             | % {
@@ -87,22 +90,22 @@ try {
 
         Write-Host "Re-generating tests"
         Invoke-Block {
-            & dotnet msbuild $PSScriptRoot/../service.proj /restore /t:GenerateTests /p:SDKType=$SDKType /p:ServiceDirectory=$ServiceDirectory
+            & dotnet msbuild $PSScriptRoot/../service.proj /restore /t:GenerateTests /p:SDKType=$SDKType /p:ServiceDirectory=$ServiceToTest
         }
     }
 
     Write-Host "Re-generating snippets"
     Invoke-Block {
-        & $PSScriptRoot\Update-Snippets.ps1 -ServiceDirectory $ServiceDirectory
+        & $PSScriptRoot\Update-Snippets.ps1 -ServiceDirectory $ServiceToTest
     }
 
     Write-Host "Re-generating listings"
     Invoke-Block {
-        & $PSScriptRoot\Export-API.ps1 -ServiceDirectory $ServiceDirectory -SDKType $SDKType -SpellCheckPublicApiSurface:$SpellCheckPublicApiSurface
+        & $PSScriptRoot\Export-API.ps1 -ServiceDirectory $ServiceToTest -SDKType $SDKType -SpellCheckPublicApiSurface:$SpellCheckPublicApiSurface
     }
 
     Write-Host "Validating installation instructions"
-    Join-Path "$PSScriptRoot/../../sdk" $ServiceDirectory  `
+    Join-Path "$PSScriptRoot/../../sdk" $ServiceToTest  `
         | Resolve-Path `
         | % { Get-ChildItem $_ -Filter "README.md" -Recurse } `
         | % {
