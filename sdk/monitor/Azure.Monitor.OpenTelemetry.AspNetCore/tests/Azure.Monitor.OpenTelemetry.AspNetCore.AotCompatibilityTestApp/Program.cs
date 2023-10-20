233b14b4e7af5966d2f4758da25e74f6d2d60d64
Timothy Mothra Lee <tilee@microsoft.com>
2023-10-20 10:18:13 -0700

    testing distro and net8

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/tests/Azure.Monitor.OpenTelemetry.AspNetCore.AotCompatibilityTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Monitor.OpenTelemetry.AspNetCore.AotCompatibilityTestApp;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
